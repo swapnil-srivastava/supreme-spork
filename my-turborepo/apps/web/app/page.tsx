2eac888177df1ffc6038d233722aa0e8202d6d72
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-01-19 17:24:24 +0000

    fix: adding the changes of the repo app name

--- my-turborepo/apps/web/app/page.tsx
+++ my-turborepo/apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@swapnilsrivastava/ui/card";
+import { Code } from "@swapnilsrivastava/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@swapnilsrivastava/ui/button";
 
 function Gradient({
   conic,
