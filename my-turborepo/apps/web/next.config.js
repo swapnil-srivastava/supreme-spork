4e80e80f034bac58caccd18b085fca484bae94b8
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-01-20 02:24:33 +0000

    fix: adding the library and turbo configuration

--- my-turborepo/apps/web/next.config.js
+++ my-turborepo/apps/web/next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@swapnilsrivastava/ui"],
+  transpilePackages: ["@swapnilsrivastava/ui, @swapnilsrivastava/library"],
 };
